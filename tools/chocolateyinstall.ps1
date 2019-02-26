487356e843d3044906c5154f8be74046e0ecb498
Balázs Keresztury <b@belidzs.hu>
2019-02-26 14:59:30 +0100

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'Log4ViewSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  file         = $fileLocation
+
+  softwareName  = 'log4view*'
+
+  checksum      = '3725244d93baabf7bfbf2d4d7a60ef282652d8c725536d85f281f106b8d4985a'
+  checksumType  = 'sha256'
+
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
